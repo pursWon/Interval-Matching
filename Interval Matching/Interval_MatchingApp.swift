2aa9970f54af3b068ad54b79e085432df74f8239
hongwongi <h>
2022-06-30 15:13:36 +0900

    Initial Commit

---
+++ Interval Matching/Interval_MatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_MatchingApp.swift
+//  Interval Matching
+//
+//  Created by MacBook Air on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct Interval_MatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Interval MatchingUITests/Interval_MatchingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Interval_MatchingUITestsLaunchTests.swift
+//  Interval MatchingUITests
+//
+//  Created by MacBook Air on 2022/06/30.
+//
+
+import XCTest
+
+class Interval_MatchingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
